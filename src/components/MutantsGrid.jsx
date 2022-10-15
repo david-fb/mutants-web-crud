35b1bf84e2649e10fc08cad330cb916a44054a6f
David Basto <davidbasto01@gmail.com>
2022-10-15 13:03:04 -0500

    update: card style

--- src/components/MutantsGrid.jsx
+++ src/components/MutantsGrid.jsx
@@ -2,17 +2,19 @@ import '../styles/MutantsGrid.css';
 import { Link } from 'react-router-dom';
 
 export default function MutantsGrid({ mutants }) {
-  return (
-    <section className="MutantsGrid">
-      {mutants.map((mutant) => (
-        <Link to={`/mutant/${mutant.id}`} key={`mutant-${mutant.id}`} className="MutantsGrid__item">
-          <img src={mutant.image} alt={mutant.name} />
-          <div className="MutantsGrid__item-info">
-            <h3>{mutant.alias}</h3>
-            <p>{mutant.name}</p>
-          </div>
-        </Link>
-      ))}
-    </section>
-  );
+  const items = mutants.map((mutant) => {
+    const type = mutant?.type === 'hero' ? 'Héroe' : 'Villano';
+    return (
+      <Link to={`/mutant/${mutant.id}`} key={`mutant-${mutant.id}`} className="MutantsGrid__item">
+        <img src={mutant.image} alt={mutant.name} />
+        <div className="MutantsGrid__item-info">
+          <p className="MutantsGrid__item-info__type">{type}</p>
+          <h3 className="MutantsGrid__item-info__alias">{mutant.alias}</h3>
+          <p className="MutantsGrid__item-info__name">{mutant.name}</p>
+        </div>
+      </Link>
+    );
+  });
+
+  return <section className="MutantsGrid">{items}</section>;
 }
