870aa7099934233547882966619fc8770efee455
David Basto <davidbasto01@gmail.com>
2022-10-15 19:10:32 -0500

    add: footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import Nav from './Nav';
+import Footer from './Footer';
 import '../styles/Layout.css';
 
 export default function Layout({ children }) {
@@ -6,6 +7,7 @@ export default function Layout({ children }) {
     <>
       <Nav />
       <main className="Main">{children}</main>
+      <Footer />
     </>
   );
 }
